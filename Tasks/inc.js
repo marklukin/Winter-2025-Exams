afcc7391c12869d1813272eafd3a816022caa48f
Mark Lukin <mark.m.lukin@gmail.com>
2025-01-09 15:54:01 +0100

    step 2

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,17 +1,17 @@
 // Refactor following solution
 // Increment all numbers in dictionary
 
-// Step 1: add 'use strict', follow naming conventions
+// Step 2: delete useless parameter, add const to the loop and rename identifiers
 
 'use strict';
 
-let incNumbers = (format_complete, ...rest_variables) => {
-  for (delete_file in format_complete) {
-    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
-      format_complete[delete_file] = format_complete[delete_file] + 1;
+let incNumbers = (dict) => {
+  for (const numbers in dict) {
+    if ((typeof dict[numbers]).charAt(0).toUpperCase() === 'N') {
+      dict[numbers] = dict[numbers] + 1;
     }
   }
-  return format_complete;
+  return dict;
 };
 
 module.exports = incNumbers;
