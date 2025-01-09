e191d3613ee08eb33304f7f1515124fb9b630bea
Mark Lukin <mark.m.lukin@gmail.com>
2025-01-09 16:56:27 +0100

    step 3

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,14 @@
 // Refactor following solution
 // Get day number
 
-//Step 2: prefer const over let
+//Step 3: create i-indentifier right in the loop
 
 'use strict';
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (str) => {
-  let i;
-  for (i = 0; i < days.length; i++) {
+  for (let i = 0; i < days.length; i++) {
     if (str.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
