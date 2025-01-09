fe271ee88bcbdda2d1d7cdd338abb0f704592e0a
Mark Lukin <mark.m.lukin@gmail.com>
2025-01-09 19:55:13 +0100

    step 2

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,14 +1,11 @@
 // Refactor following solution
 // Sum all numbers from an array
 
-//Step 1: add 'use strict', 
-// fix the formatting of the main function
+//Step 2: remove senseless lines
 
 'use strict';
 
 const sum = (arr) => {
-  sum = [0];
-  let k = 5;
   for (i of arr) {
     let t = typeof i;
     if (t === 'number') {
@@ -18,9 +15,7 @@ const sum = (arr) => {
       }
     }
   }
-  sum[0];
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
 };
 
 module.exports = sum;
