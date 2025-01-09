eea21c9b26f7f30e993df445b78a9f732942537b
Mark Lukin <mark.m.lukin@gmail.com>
2025-01-09 20:03:33 +0100

    step 4 (done)

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,16 +1,14 @@
 // Refactor following solution
 // Sum all numbers from an array
 
-//Step 3: add const to for..of loop, 
-// instead of new_Sum, create container result
+//Step 4: optimise the loop
 
 'use strict';
 
 const sum = (arr) => {
   let result = 0;
   for (const key of arr) {
-    let t = typeof key;
-    if (t === 'number') {
+    if (typeof key === 'number') {
       result += key;
     }
   }
