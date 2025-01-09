51d8af6acf5f4db18724acfe14db073f464965c9
Mark Lukin <mark.m.lukin@gmail.com>
2025-01-09 15:47:44 +0100

    step 1, starting

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,7 +1,11 @@
 // Refactor following solution
 // Increment all numbers in dictionary
 
-let inc_numbers = (format_complete, ...rest_variables) => {
+// Step 1: add 'use strict', follow naming conventions
+
+'use strict';
+
+let incNumbers = (format_complete, ...rest_variables) => {
   for (delete_file in format_complete) {
     if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
       format_complete[delete_file] = format_complete[delete_file] + 1;
@@ -10,4 +14,4 @@ let inc_numbers = (format_complete, ...rest_variables) => {
   return format_complete;
 };
 
-module.exports = inc_numbers;
+module.exports = incNumbers;
