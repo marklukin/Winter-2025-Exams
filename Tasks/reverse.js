ff584b7f7a277dfb0cd9a56f3c59ef64267b004a
Mark Lukin <mark.m.lukin@gmail.com>
2025-01-10 15:09:51 +0100

    step 2

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,18 +1,16 @@
 // Refactor following solution
 // Reverse dict, exchange keys and values
 
-// Step 1: add 'use strict', follow naming conventions, 
-// remove senseless line
+// Step 2: use for..of loop, instead of forEach
 
 'use strict';
 
 const reverse = (dict) => {
-  T = Object.keys(dict, 500);
-  T.forEach((_) => {
-    const v1 = dict[_];
-    dict[v1] = _;
-    delete dict[_];
-  }, 1000);
+  for (const key of Object.keys(dict)) {
+    const v1 = dict[key];
+    dict[v1] = key;
+    delete dict[key];
+  }
   return dict;
 };
 
