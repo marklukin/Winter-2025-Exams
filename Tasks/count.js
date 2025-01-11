8d1609c9170efa71191b9cc00dc473aa62eb2653
Mark Lukin <mark.m.lukin@gmail.com>
2025-01-11 14:42:09 +0100

    step 2

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,21 +1,17 @@
 // Refactor following solution
 // Sum all number values in dict
 
-// Step 1: add 'use strict', add const
+// Step 2: remove senseless blocks, add let to identifier
 
 'use strict';
 
 const count = (obj) => {
-  sum = 0;
-  {
-  }
+  let sum = 0;
   keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
